98c9cb730333e067a570d0b1ed842b31fd5542f1
Danny-15-sys <momodudaniel16@gmail.com>
2024-10-21 18:19:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,13 +10,13 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "242067712688"
+global.ownernomer = "2349019119466"
 global.ownername = "Satoru"
 global.ytname = "YT: SatoruTech"
 global.socialm = "GitHub: SatoruTech"
 global.location = "Congo-Brazzaville, Brazzaville, null"
 
-global.ownernumber = '242067712688'  //creator number
+global.ownernumber = '2349019119466'  //creator number
 global.ownername = 'SatoruTech' //owner name
 global.botname = 'Satoru-Bug-Bot' //name of the bot
 
